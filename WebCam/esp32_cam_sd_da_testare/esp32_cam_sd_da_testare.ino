764bd2398a180f2869fc4732a3f4a3583487da41
CorroSamuele <corro.samuele@gmail.com>
2025-05-24 11:46:18 +0200

    WebCam

---
+++ WebCam/esp32_cam_sd_da_testare/esp32_cam_sd_da_testare.ino
@@ -0,0 +1,103 @@
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <WebServer.h>
+
+// Configura i pin della camera (AI-Thinker)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM     0
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      21
+#define Y4_GPIO_NUM      19
+#define Y3_GPIO_NUM      18
+#define Y2_GPIO_NUM       5
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+
+
+// Inserisci qui SSID e password WiFi
+const char* ssid = "WifiCasaCorro2.4_EXT";
+const char* password = "WifiCasaCorro64699500";
+
+WebServer server(80);
+
+void handle_capture() {
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    server.send(500, "text/plain", "Errore acquisizione immagine");
+    return;
+  }
+
+  // Scrive manualmente l’header HTTP
+  server.sendHeader("Content-Type", "image/jpeg");
+  server.sendHeader("Content-Length", String(fb->len));
+  server.send(200);
+  WiFiClient client = server.client();
+  client.write(fb->buf, fb->len);
+
+  esp_camera_fb_return(fb);
+}
+
+void startCameraServer() {
+  server.on("/", handle_capture);
+  server.begin();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Errore inizializzazione camera");
+    return;
+  }
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connesso. IP:");
+  Serial.println(WiFi.localIP());
+
+  startCameraServer();
+}
+
+void loop() {
+  server.handleClient();
+}
